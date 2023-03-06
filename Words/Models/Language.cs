48199bc2526d0d40fdcf309df0c858dd3d2f13bd
Victor Bozhuk <bozhook@gmail.com>
2023-03-06 18:35:18 +0200

    added stased method

--- Words/Models/Language.cs
+++ Words/Models/Language.cs
@@ -30,5 +30,10 @@ namespace words.Models
         {
             return $"{Title} : {getAverageLevel()}";
         }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
     }
 }
